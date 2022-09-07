f59e43ebcce3739f9d63f1bf842efa43a0440493
LAB MITE <106295861+sslabmite174@users.noreply.github.com>
2022-09-07 14:07:27 +0530

    Create Max_of_two_numbers_without_condition.java

---
+++ Max_of_two_numbers_without_condition.java
@@ -0,0 +1,13 @@
+
+public class NewFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=6;
+		int y=7;
+		int max = (int)((x+y)/2.0 +Math.abs(x-y)/2.0);
+		System.out.print(max);
+		
+	}
+
+}
